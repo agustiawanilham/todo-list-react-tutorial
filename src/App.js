83baca3d6dcb3593085a4b02e55a73bc9ef4ab6c
Ilham Agustiawan <agustiawan.ilham@gmail.com>
2020-02-25 22:48:55 +0700

    Innitial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: 'Hello World',
+      newTodo:'',
+      todos:[
+        {
+          title:"Bersih-bersih",
+          done:false
+        },
+        {
+          title:"Gosok gigi",
+          done:false
+        }
+
+      ],
+    };
+  }
+  formSubmitted(event){
+    event.preventDefault();
+   
+    this.setState({
+      newTodo:'',
+      todos:[...this.state.todos, {
+        title: this.state.newTodo,
+        done:false,
+      }]
+    });
+  };
+  newTodoChanged(event){
+    this.setState({
+      newTodo:event.target.value,
+    })    
+  };
+  toggleTodoDone(event, index) {
+    const todos = [...this.state.todos];
+    todos[index] = {...todos[index]};
+    todos[index].done = event.target.checked;
+    this.setState({
+      todos
+    });
+    console.log(todos);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h3>{this.state.message }</h3>
+        <form onSubmit={(event)=>this.formSubmitted(event)}>
+          <label htmlFor="newTodo">New Todo</label>
+          <input onChange={(event)=>this.newTodoChanged(event)} id="newTodo" name="newTodo" value={this.state.newTodo}/>
+          <button type="submit">Add Todo</button>
+        </form>
+        <ul>
+          {this.state.todos.map((todo, index)=> {
+            return(
+            <li key={todo.title}>
+              <input onChange={(event)=>this.toggleTodoDone(event, index)} type="checkbox"/> 
+              <span style={{textDecoration: todo.done ? 'line-through' : 'inherit'}}>
+                {todo.title}
+              </span>
+            </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
